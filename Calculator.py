3770abe9b81dee94c344ed551508a1935b0c2e43
Wallace-Belo <54925524+Wallace-Belo@users.noreply.github.com>
2020-09-18 15:23:31 -0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+#Calculadora
+
+while (True):
+    print ("\n\t ---Calculator--- \n")
+    n1 = int (input("Input the first number: "))
+    n2 = int (input("Input the second: "))
+    print("\n\t //---Results---// \n")
+    #--
+    print("Addition: ", n1+n2)
+    #--
+    print("\n Subtraction: ", n1-n2)
+    #--
+    print("\n Multiplication: ", n1*n2)
+    #--
+    print("\n Division: ", n1/n2)
